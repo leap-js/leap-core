4d04c972ff3ec8f8d73712ffb5f3f8a260cc1569
Giani Segatto <gsegatto@travix.com>
2019-05-25 17:14:57 +0100

    exporting component

--- src/main/node/functions.js
+++ src/main/node/functions.js
@@ -2,8 +2,10 @@
 
 const EnvironmentLoader = require("./environment/EnvironmentLoader");
 const ErrorBuilder = require("./error/ErrorBuilder");
+const Component = require("./domain/Component");
 
 exports = module.exports;
 
 exports.ErrorBuilder = ErrorBuilder;
 exports.EnvironmentLoader = EnvironmentLoader;
+exports.Component = Component;
